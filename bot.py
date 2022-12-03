3982acba39148169b6a260c0d6a63ccf5743d6fa
textil24 <textilofficial@mail.ru>
2022-12-03 22:47:40 +0300

    added many functions which create actual test!
    added new file test_code.py I gonna changded code under principle DRY and KISS and then changed bot.py under this code

--- bot.py
+++ bot.py
@@ -1,15 +1,81 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import types, executor, Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 TOKEN_API = '5494716985:AAGT8sfi28BG21qTDJD4rvu9ki5uCjiRSzY'
 
+storage = MemoryStorage()
+
 bot = Bot(TOKEN_API)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
+questions_and_answers = {
+    'Что такое Python?': 'язык программирования',
+    'C помощью какой функции (метода) в модуле itertools возможно последовательно перебирать элементы iterable-объекта бесконечно? Напишите название функции без скобок с учетом регистра?': 'count()',
+    'Назовите шаблоны проектирования, относящиеся к группе порождающих:': 'фабричный метод, фасад, строитель, одиночка, декоратор',
+}
 
 
-@dp.message_handler()
-async def cmd_start(message: types.Message):
-    await message.answer("Hello world!")
+class ProfileStatesGroup(StatesGroup):
+
+    question1 = State()
+    question2 = State()
+    question3 = State()
+
+
+def get_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton('/test'))
+
+    return kb
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message) -> None:
+    await message.answer('<b>Welcome to Test!</b> Click to start the test - /test',
+                         reply_markup=get_kb(), parse_mode='html')
+
+keys = list(questions_and_answers)
+
+
+@dp.message_handler(commands=['test'])
+async def cmd_create(message: types.Message) -> None:
+    await message.reply(keys[0])
+    await ProfileStatesGroup.question1.set()
+
+
+@dp.message_handler(state=ProfileStatesGroup.question1)
+async def load_name(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['question1'] = message.text
+
+    if message.text == questions_and_answers[keys[0]]:
+        await message.reply(keys[1])
+        await ProfileStatesGroup.next()
+
+
+@dp.message_handler(state=ProfileStatesGroup.question2)
+async def load_name(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['question2'] = message.text
+
+    if message.text == questions_and_answers[keys[1]]:
+        await message.reply(keys[2])
+        await ProfileStatesGroup.next()
+
+
+@dp.message_handler(state=ProfileStatesGroup.question3)
+async def load_desc(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['question3'] = message.text
+        print(data)
+
+    if message.text == questions_and_answers[keys[2]]:
+        await message.reply('<b>Конец теста!!!</b>', parse_mode='html')
+        await state.finish()
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
