784848284649204cbb3f8f873c1497e7e0670f4b
textil24 <textilofficial@mail.ru>
2022-12-11 21:26:35 +0300

    added refinement of the code and adding hints

--- bot.py
+++ bot.py
@@ -1,27 +1,175 @@
-from aiogram import types, executor, Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+import random
 
-from checker_tests import handlers
-from checker_question import handler
-from keyboards import keyboards
-from preview_bot import start, help
+import aioschedule
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+
+import json
 
 TOKEN_API = '5886107638:AAHuzhRErcTNw2RCnmHyF79BCXsxcvDo284'
+
+user_id = ''
+text = ''
+count = 0
 
 storage = MemoryStorage()
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot, storage=storage)
 
-keyboards.get_kb()
 
-start.start_command(dp)
-help.help_command(dp)
+# keyboards.get_kb()
+
+# start.start_command(dp)
+# help.help_command(dp)
 
 # handler.handler(dp)
-handlers.handlers(dp)
+# handlers.handlers(dp)
 
 
-if __name__ == "__main__":
+with open("checker_tests/data/questions_and_answers.json", encoding='utf-8') as file:
+    src = json.load(file)
+
+list_questions_answers = [src['question_and_answers'][item] for item in src['question_and_answers']]
+questions_and_answers = {}
+for quest in list_questions_answers:
+    questions_and_answers[tuple(quest)[0]] = quest[tuple(quest)[0]]
+
+keys = list(questions_and_answers)
+
+
+class ProfileStatesGroup(StatesGroup):
+
+    text = State()
+
+
+class ProfileStatesGroup1(StatesGroup):
+
+    question = State()
+
+
+@dp.message_handler(commands=['start'])
+async def choose_your_dinner(message: types.Message):
+    global user_id
+    user_id = message.from_user.id
+    global text
+    text = message.text
+    await bot.send_message(user_id, 'Введите время ( в секундах ) для получения вопроса:')
+    await ProfileStatesGroup.next()
+
+
+@dp.message_handler(state=ProfileStatesGroup.text)
+async def load_name(message: types.Message, state: FSMContext) -> None:
+    # await scheduler('16:29')
+    await state.finish()
+    await scheduler(int(message.text))
+
+
+async def question_test():
+    global id
+    id = random.randint(0, len(keys) - 1)
+
+    if photo := questions_and_answers[keys[id]][1]:
+        print('Я работаю')
+        photo_init = open(photo, 'rb')
+        await bot.send_photo(user_id, photo_init, caption=keys[id], parse_mode='html')
+        # await bot.send_message(user_id,
+        #     text=f'Тра-та-та{hide_link(photo)}',
+        #     parse_mode='HTML'
+        # )
+        await ProfileStatesGroup1.next()
+    else:
+        await bot.send_message(user_id, keys[id], parse_mode='html')
+        await ProfileStatesGroup1.next()
+
+@dp.message_handler(state=ProfileStatesGroup1.question)
+async def load_name(message: types.Message, state: FSMContext) -> None:
+    if message.text.lower().replace(' ', '') in questions_and_answers[keys[id]][0]:
+        if message.text in questions_and_answers[keys[id]][0]:
+            await message.answer('<b>Совершенно верно! Продолжай в том же духе...</b>', parse_mode='html')
+            await bot.send_sticker(user_id, sticker=questions_and_answers[keys[id]][2])
+        else:
+            await message.answer(f'<b>Это похоже на правильный ответ!</b> В следующий раз пишите: <b>{questions_and_answers[keys[id]][0][-1]}</b>', parse_mode='html')
+            await bot.send_sticker(user_id, sticker=questions_and_answers[keys[id]][2])
+    elif message.text == '/secretend':
+        global text
+        text = message.text
+        await message.answer('<b>Конец вопроса!!!</b>', parse_mode='html')
+        await state.finish()
+    else:
+        global count
+        count += 1
+        if count in [3, 4]:
+            await message.answer(questions_and_answers[keys[id]][3], parse_mode='html')
+        elif count in [5]:
+            await message.answer('<b>Неправильный ответ! Попробуйте снова...</b>', parse_mode='html')
+            count = 0
+
+        else:
+            await message.answer('<b>Неправильный ответ! Попробуйте снова...</b>', parse_mode='html')
+
+
+async def scheduler(time):
+    # aioschedule.every().day.at(time).do(do_get)
+    aioschedule.every(time).seconds.do(question_test)
+
+    while True:
+        print('Отработал!')
+        await aioschedule.run_pending()
+        await asyncio.sleep(time)
+        end_script = text
+        if end_script == '/secretend':
+            end_script = ''
+            break
+
+# async def get_data(**kwargs):
+#     fruits = [
+#         ("Фабричный метод", '1'),
+#         ("Фасад", '2'),
+#         ("Строитель", '3'),
+#         ("Одиночка", '4'),
+#         ("Декоратор", '5'),
+#     ]
+#     return {
+#         "fruits": fruits,
+#         "count": len(fruits),
+#     }
+#
+#
+# async def bots_filter_changed(c: CallbackQuery, multiselect_adapter: ManagedWidgetAdapter,
+#                               dialog_manager: DialogManager, item_id: str):
+#     print("Filter changed: ", item_id)
+#
+#
+# fruits_kbd = Multiselect(
+#     Format("✓ {item[0]}"),
+#     Format("{item[0]}"),
+#     id="m_fruits",
+#     item_id_getter=operator.itemgetter(1),
+#     items="fruits",
+#     on_state_changed=bots_filter_changed,
+# )
+#
+# column = Column(
+#     fruits_kbd,
+# )
+#
+# main_window = Window(
+#     Const("Question"),
+#     column,
+#     state=MySG.main,
+#     getter=get_data,
+# )
+# dialog = Dialog(main_window)
+# registry.register(dialog)
+#
+#
+# @dp.message_handler(commands=["button"])
+# async def start(m: Message, dialog_manager: DialogManager):
+#     await dialog_manager.start(MySG.main, mode=StartMode.RESET_STACK)
+
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
