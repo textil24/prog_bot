7ef542c65e9831dc36813325ef5a0538d63caffa
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2022-12-14 23:23:16 +0300

    changes

--- bot.py
+++ bot.py
@@ -12,9 +12,7 @@ import json
 
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
 from aiogram_dialog import DialogManager, StartMode, DialogRegistry, Dialog, Window
-from aiogram_dialog.widgets.kbd import SwitchTo, Multiselect, Column
-from aiogram_dialog.widgets.managed import ManagedWidgetAdapter
-from aiogram_dialog.widgets.text import Const, Format
+
 
 TOKEN_API = '5886107638:AAHuzhRErcTNw2RCnmHyF79BCXsxcvDo284'
 
@@ -35,6 +33,9 @@ registry = DialogRegistry(dp)
 
 # handler.handler(dp)
 # handlers.handlers(dp)
+
+BEFORE_STOP_BOT = "Бот остановлен"
+question_number = 0
 
 
 with open("checker_tests/data/questions_and_answers.json", encoding='utf-8') as file:
@@ -62,8 +63,7 @@ class SelectState(StatesGroup):
 
 
 @dp.message_handler(commands=["start"])
-async def start(message: Message, ):
-    global id
+async def start(message: Message):
     global user_id
     user_id = message.from_user.id
     global text
@@ -80,6 +80,11 @@ async def load_name(message: types.Message, dialog_manager: DialogManager, state
     await scheduler(message, dialog_manager, int(message.text))
 
 
+@dp.message_handler(commands=["/theory"])
+async def send_theory(message: types.Message):
+    await message.answer("Hello, <b>world</b>!", parse_mode="HTML")
+
+
 async def scheduler(message: types.Message, dialog_manager: DialogManager, time):
     # aioschedule.every().day.at(time).do(do_get)
     aioschedule.every(time).seconds.do(lambda: question_test(message, dialog_manager))
@@ -93,52 +98,38 @@ async def scheduler(message: types.Message, dialog_manager: DialogManager, time)
             break
 
 
-async def question_test(message: Message, dialog_manager: DialogManager):
-    global id
-    if id < len(keys) - 1:
-        # if id == 0:
-        #     print("first question send")
-        #     await SelectState.b.set()
-        #     await dialog_manager.start(SelectState.b, reset_stack=True)
-        #     await PsgQuestion.next()
-        if photo := questions_and_answers[keys[id]][1]:
+async def question_test():
+    global question_number
+    if question_number < len(keys) - 1:
+        if photo := questions_and_answers[keys[question_number]][1]:
             photo_init = open(photo, 'rb')
-            await bot.send_photo(user_id, photo_init, caption=keys[id], parse_mode='html')
+            await bot.send_photo(user_id, photo_init, caption=keys[question_number], parse_mode='html')
             await PsgQuestion.next()
         else:
-            await bot.send_message(user_id, keys[id], parse_mode='html')
+            await bot.send_message(user_id, keys[question_number], parse_mode='html')
             await PsgQuestion.next()
-        id = id + 1
+        question_number += 1
 
 
 @dp.message_handler(state=PsgQuestion.question)
 async def load_name(message: types.Message, state: FSMContext) -> None:
     if message.text.lower().replace(' ', '') in questions_and_answers[keys[id]][0]:
-        print(keys)
-        print(id)
-        if message.text in questions_and_answers[keys[id]][0]:
-            await message.answer('<b>Совершенно верно! Продолжай в том же духе...</b>', parse_mode='html')
-            await bot.send_sticker(user_id, sticker=questions_and_answers[keys[id]][2])
-        else:
-            await message.answer(
-                f'<b>Это похоже на правильный ответ!</b> В следующий раз пишите: <b>{questions_and_answers[keys[id]][0][-1]}</b>',
-                parse_mode='html')
-            await bot.send_sticker(user_id, sticker=questions_and_answers[keys[id]][2])
-    elif message.text == '/secretend':
-        global text
-        text = message.text
-        await message.answer('<b>Конец вопроса!!!</b>', parse_mode='html')
-        await state.finish()
+        await message.answer('<b>Совершенно верно! Продолжай в том же духе...</b>', parse_mode='html')
+        await bot.send_sticker(user_id, sticker=questions_and_answers[keys[id]][2])
+    # elif message.text == '/stop':
+    #     global text
+    #     text = message.text
+    #     await message.answer(BEFORE_STOP_BOT, parse_mode='html')
+    #     await state.finish()
     else:
-        global count
-        count += 1
-        if count in [3, 4]:
-            await message.answer(questions_and_answers[keys[id]][3], parse_mode='html')
-        elif count in [5]:
-            await message.answer('<b>Неправильный ответ! Попробуйте снова...</b>', parse_mode='html')
-            count = 0
-
-        else:
+        # global count
+        # count += 1
+        # if count in [3, 4]:
+        #     await message.answer(questions_and_answers[keys[id]][3], parse_mode='html')
+        # elif count in [5]:
+        #     await message.answer('<b>Неправильный ответ! Попробуйте снова...</b>', parse_mode='html')
+        #     count = 0
+        # else:
             await message.answer('<b>Неправильный ответ! Попробуйте снова...</b>', parse_mode='html')
 
 
