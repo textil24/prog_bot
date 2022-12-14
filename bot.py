254d1e19a347251b4dbd372d6401f286a4870e86
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2022-12-15 00:49:54 +0300

    changes

--- bot.py
+++ bot.py
@@ -33,13 +33,23 @@ registry = DialogRegistry(dp)
 # handlers.handlers(dp)
 
 BEFORE_STOP_BOT = "Бот остановлен"
-question_number = 0
+CORRECT_ANSWER = '<b>Совершенно верно! Продолжай в том же духе!</b>'
+WRONG_ANSWER = '<b>Неправильный ответ! Попробуйте снова...</b>'
+question_number = -1
 
 with open("checker_tests/data/questions_and_answers.json", encoding='utf-8') as file:
     src = json.load(file)
 
 list_questions_answers = [src['question_and_answers'][item] for item in src['question_and_answers']]
 questions_and_answers = {}
+answers = [["134", "143", "341", "431", "314", "413"],
+           ["count", "Сount"],
+           ["конструктор", "Конструктор"],
+           ["декоратор", "Декоратор"],
+           ["self", "Self"],
+           ["stopiteration", "StopIteration", "stopIteration"],
+           ["итератор", "Итератор"],
+           ["cинглтон", "Синглтон"]]
 for quest in list_questions_answers:
     questions_and_answers[tuple(quest)[0]] = quest[tuple(quest)[0]]
 
@@ -83,7 +93,7 @@ async def send_theory(message: types.Message):
 
 
 async def scheduler(time):
-   #aioschedule.every().day.at(time).do(do_get)
+    # aioschedule.every().day.at(time).do(do_get)
     aioschedule.every(time).seconds.do(send_question_test)
 
     while True:
@@ -97,8 +107,8 @@ async def scheduler(time):
 
 async def send_question_test():
     global question_number
+    question_number += 1
     print("question number {}".format(question_number))
-    print(keys)
     if question_number < len(keys) - 1:
         if photo := questions_and_answers[keys[question_number]][1]:
             photo_init = open(photo, 'rb')
@@ -107,18 +117,17 @@ async def send_question_test():
         else:
             await bot.send_message(user_id, keys[question_number], parse_mode='html')
             await PsgQuestion.next()
-        question_number += 1
 
 
 @dp.message_handler(state=PsgQuestion.question)
 async def load_name(message: types.Message, state: FSMContext) -> None:
-    if message.text.lower().replace(' ', '') in questions_and_answers[keys[question_number]][0]:
-        await message.answer('<b>Совершенно верно! Продолжай в том же духе...</b>', parse_mode='html')
+    user_answer = message.text.lower().replace(' ', '')
+    if user_answer in answers[question_number]:
+        await message.answer(CORRECT_ANSWER, parse_mode='html')
         await bot.send_sticker(user_id, sticker=questions_and_answers[keys[question_number]][2])
     else:
-        await message.answer('<b>Неправильный ответ! Попробуйте снова...</b>', parse_mode='html')
+        await message.answer(WRONG_ANSWER, parse_mode='html')
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
